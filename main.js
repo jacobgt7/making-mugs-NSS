11463740933c46470013b22cb8513901b41c36ec
Jacob Thomas <jacgretho@gmail.com>
2022-10-07 10:32:07 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (let lump of clay) {
+   const mug = `${lump} coffee mug`
+   toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln)
